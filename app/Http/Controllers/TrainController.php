7c761688d72ba0143cb6f6b0e2a6968dc9f323a1
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-11 15:40:58 +0200

    feat: controller, model and route updated/created

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+        $trainList = Train::all();
+        dd($trainList);
+
+        return view('home');
+    }
+}
