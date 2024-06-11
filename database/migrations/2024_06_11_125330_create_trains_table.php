fc00528ebceb7b69505f9bd34d7695e6d284024b
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-11 15:27:55 +0200

    feat: table column created

---
+++ database/migrations/2024_06_11_125330_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            
+            $table->id();
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->smallInteger('train_number')->unsigned();
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('is_on_time');
+            $table->boolean('canceled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
