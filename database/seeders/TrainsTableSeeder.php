ebe4c0162d8c06e645bbb2e59ed0388ca68b7e44
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-12 15:35:39 +0200

    feat: it localization implemented

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,20 +5,21 @@ namespace Database\Seeders;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
-
+// use Faker\Generator as Faker;
+use Faker;
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker): void
-    {
+    // public function run(Faker $faker): void {
+    public function run(): void {
         for ($i = 0; $i < 10; $i++) {
+            $faker = Faker\Factory::create('it_IT');
             $newTrain = new Train();
             $newTrain->departure_station = $faker->streetAddress();
             $newTrain->arrival_station = $faker->streetAddress();
-            $newTrain->departure_date = $faker->dateTimeBetween('-4 days', '+2 week');
+            $newTrain->departure_date = $faker->dateTimeBetween('-4 days', '+1 week');
             $newTrain->departure_time = $faker->time();
             $newTrain->arrival_time = $faker->time();
             $newTrain->train_number = $faker->randomNumber(3, true);
